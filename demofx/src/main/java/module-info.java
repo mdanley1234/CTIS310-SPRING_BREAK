4ca2a572aaa4373c11c3684aebc085a9aebabd40
Michael Danley <112212314+mdanley1234@users.noreply.github.com>
2025-03-12 13:25:38 -0400

    Delte

--- demofx/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module edu.guilford {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-    opens edu.guilford to javafx.fxml;
-    exports edu.guilford;
-}
