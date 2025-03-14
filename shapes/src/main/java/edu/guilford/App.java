f90e79bfe425ac0c24fbff35d4ab00323c518775
Michael Danley <112212314+mdanley1234@users.noreply.github.com>
2025-03-14 14:15:20 -0400

    asd

--- shapes/src/main/java/edu/guilford/App.java
+++ shapes/src/main/java/edu/guilford/App.java
@@ -1,12 +1,12 @@
 package edu.guilford;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * JavaFX App
@@ -29,6 +29,7 @@ public class App extends Application {
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
+        
     }
 
     public static void main(String[] args) {
--- shapes/src/main/java/edu/guilford/PrimaryController.java
+++ shapes/src/main/java/edu/guilford/PrimaryController.java
@@ -1,12 +1,37 @@
 package edu.guilford;
 
 import java.io.IOException;
+
 import javafx.fxml.FXML;
 
 public class PrimaryController {
 
     @FXML
+    private javafx.scene.control.TextField firstNameBox;
+
+    @FXML
+    private javafx.scene.control.TextField lastNameBox;
+
+    @FXML
+    private javafx.scene.control.TextField ageBox;
+
+    @FXML
+    private javafx.scene.control.TextField countryBox;
+
+    @FXML
+    private javafx.scene.control.TextField heightBox;
+
+    @FXML
+    private javafx.scene.control.Label headerLabel;
+
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @FXML
+    private void updateName() throws IOException {
+        String firstName = firstNameBox.getText();
+        headerLabel.setText("Welcome " + firstName);
     }
 }
